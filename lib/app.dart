73605fd7d043158d37f2c8895043718a6626a218
kenjinezumi <kenjitsuchiya1@gmail.com>
2025-02-09 21:01:17 +0000

    Modified app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,30 @@
+// lib/app.dart
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'splash_screen.dart';
+import 'providers/auth_provider.dart';
+import 'providers/booking_provider.dart';
+import 'providers/rating_provider.dart';
 
 class LittleGuardianApp extends StatelessWidget {
   const LittleGuardianApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LittleGuardian',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: const Scaffold(
-        body: Center(child: Text("Hello from LittleGuardian!")),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
+        ChangeNotifierProvider<BookingProvider>(create: (_) => BookingProvider()),
+        ChangeNotifierProvider<RatingProvider>(create: (_) => RatingProvider()),
+      ],
+      child: MaterialApp(
+        title: 'LittleGuardian',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
       ),
     );
   }
