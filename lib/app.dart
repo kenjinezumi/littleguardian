48e9b6148ee3555a5809f6fe8bd1bc74dfa8ec39
kenjinezumi <kenjitsuchiya1@gmail.com>
2025-02-10 01:05:15 +0000

    Fixes

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,15 @@
 // lib/app.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
+// or whatever references you have
 
 import 'splash_screen.dart';
+import './pages/login_page.dart';
+import './pages/home_page.dart';
+import './pages/phone_auth_page.dart';
+import './pages/otp_page.dart';
+
 import 'providers/auth_provider.dart';
 import 'providers/booking_provider.dart';
 import 'providers/rating_provider.dart';
@@ -19,12 +26,23 @@ class LittleGuardianApp extends StatelessWidget {
         ChangeNotifierProvider<RatingProvider>(create: (_) => RatingProvider()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'LittleGuardian',
         theme: ThemeData(
           primarySwatch: Colors.teal,
           useMaterial3: true,
         ),
-        home: const SplashScreen(),
+        // We can use home: or initialRoute: or both
+        // Let's demonstrate using routes + initialRoute:
+        initialRoute: '/',
+        routes: {
+          // The splash screen is our initial route:
+          '/': (ctx) =>  SplashScreen(),
+          '/login': (ctx) =>  LoginPage(),
+          '/home': (ctx) =>  HomePage(),
+          '/phone': (ctx) =>  PhoneAuthPage(),
+          '/otp': (ctx) =>  OtpPage(),
+        },
       ),
     );
   }
